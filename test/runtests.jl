3b81e1b39e2cf7130f1d487eaacf5c6cf07cc34d
Paul Bayer <Paul.Bayer@gleichsam.de>
2021-01-05 09:09:45 +0100

    first tests for error handling

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,14 +15,15 @@ end
 
 length(procs()) == 1 && addprocs(1)
 
-@safetestset "Basics"        begin include("test_basics.jl") end
-@testset "Distributed"       begin include("test_distr.jl") end
-@safetestset "Communication" begin include("test_com.jl") end
-@safetestset "API"           begin include("test_api.jl") end
-@safetestset "Diagnosis"     begin include("test_diag.jl") end
-@safetestset "Tasks"         begin include("test_task.jl") end
-@testset "Registry"          begin include("test_registry.jl") end
-@safetestset "Utilities"     begin include("test_utils.jl") end
+@safetestset "Basics"         begin include("test_basics.jl") end
+@testset "Distributed"        begin include("test_distr.jl") end
+@safetestset "Communication"  begin include("test_com.jl") end
+@safetestset "Error handling" begin include("test_errorhandling.jl") end
+@safetestset "API"            begin include("test_api.jl") end
+@safetestset "Diagnosis"      begin include("test_diag.jl") end
+@safetestset "Tasks"          begin include("test_task.jl") end
+@testset "Registry"           begin include("test_registry.jl") end
+@safetestset "Utilities"      begin include("test_utils.jl") end
 
 println("running examples, output suppressed!")
 redirect_devnull() do
